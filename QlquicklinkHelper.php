589c18bd0cc8b19f6adefbf13607268a32830866
hoochicken <mareike.riegel@ql.de>
2023-10-24 08:15:33 +0200

    fix

--- QlquicklinkHelper.php
+++ QlquicklinkHelper.php
@@ -45,10 +45,10 @@ class QlquicklinkHelper
         $style = '';
         $style .= sprintf('.qlquicklink {top:%spx;right:%spx;z-index:%s;}', $params->get('margin_top', 30), $params->get('margin_right', 0), $params->get('zindex', 5));
         $style .= sprintf('.qlquicklink ul li {margin-bottom: %spx;}', $params->get('margin_button_bottom', 30));
+        $style .= sprintf('.qlquicklink ul li.link-visible {color: %s;background-color:%s;}', $params->get('strip_color', '#30a6dc'), $params->get('strip_background_color', '#fffff'));
         $style .= sprintf('.qlquicklink ul li img {width:%spx;}', $params->get('image_width', 30));
-        $style .= sprintf('.qlquicklink ul li i::before {font-size:%spx;color:%s;}', $params->get('icon_size', 30), $params->get('color', '#ffffff'));
-        $style .= sprintf('.qlquicklink ul li a.btn {background:%s;}', $params->get('background_color', '#30a6dc'));
-        // $style .= sprintf('', $params->get('background_color', '#30a6dc'));
+        $style .= sprintf('.qlquicklink ul li i::before {font-size:%spx;color:%s;}', $params->get('icon_size', 30), $params->get('icon_color', '#ffffff'));
+        $style .= sprintf('.qlquicklink ul li a.btn {background:%s;}', $params->get('icon_background_color', '#30a6dc'));
         return $style;
     }
 
