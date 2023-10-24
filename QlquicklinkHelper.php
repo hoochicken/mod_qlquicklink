a1503f40bce304077741b2a29d9bf61b9542db4c
hoochicken <mareike.riegel@ql.de>
2023-10-24 17:26:07 +0200

    fix (js) works now basically

--- QlquicklinkHelper.php
+++ QlquicklinkHelper.php
@@ -18,9 +18,10 @@ class QlquicklinkHelper
 {
     public Registry $params;
     public stdClass $module;
+    public int $width = 200;
     public int $numberOfButtons = 5;
 
-    function __construct($module, $params)
+    public function __construct($module, $params)
     {
         $this->module = $module;
         $this->params = $params;
@@ -42,13 +43,20 @@ class QlquicklinkHelper
 
     public function getStyles(Registry $params): string
     {
+        $liHeight = 50;
+        $liMarginLeft = $this->width - $liHeight - 30;
+
         $style = '';
         $style .= sprintf('.qlquicklink {top:%spx;right:%spx;z-index:%s;}', $params->get('margin_top', 30), $params->get('margin_right', 0), $params->get('zindex', 5));
-        $style .= sprintf('.qlquicklink ul li {margin-bottom: %spx;}', $params->get('margin_button_bottom', 30));
-        $style .= sprintf('.qlquicklink ul li.link-visible {color: %s;background-color:%s;}', $params->get('strip_color', '#30a6dc'), $params->get('strip_background_color', '#fffff'));
+        $style .= sprintf('.qlquicklink ul {width:%spx;}', $params->get('width', $this->width));
+        $style .= sprintf('.qlquicklink ul li {margin-bottom: %spx;width:%spx;margin-left:%spx;}', $params->get('margin_button_bottom', 30), $this->width, $liMarginLeft);
+        $style .= sprintf('.qlquicklink ul li.link-visible {color: %s;background-color:%s;}', $params->get('strip_color', '#30a6dc'), $params->get('strip_background_color', '#ffffff'));
         $style .= sprintf('.qlquicklink ul li img {width:%spx;}', $params->get('image_width', 30));
         $style .= sprintf('.qlquicklink ul li i::before {font-size:%spx;color:%s;}', $params->get('icon_size', 30), $params->get('icon_color', '#ffffff'));
         $style .= sprintf('.qlquicklink ul li a.btn {background:%s;}', $params->get('icon_background_color', '#30a6dc'));
+        $style .= sprintf('.qlquicklink ul li a.btn {background:%s;}', $params->get('icon_background_color', '#30a6dc'));
+        $style .= sprintf('.qlquicklink ul li a.btn {background:%s;}', $params->get('icon_background_color', '#30a6dc'));
+
         return $style;
     }
 
